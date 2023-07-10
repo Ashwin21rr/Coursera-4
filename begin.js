47604137377c3736db82938cd7eec7e778285a43
Ashwin21rr <137504759+Ashwin21rr@users.noreply.github.com>
2023-07-10 10:05:07 +0530

    Add files via upload

---
+++ begin.js
@@ -0,0 +1,11 @@
+function greetuser(name)
+{   let msg="Hello"+name
+    console.log(msg)
+}
+greetuser("Hover")
+let name="juilet"
+let msg="Good bye"+ name;
+console.log(msg)
+let name1="Danial"
+let text="Hello"+name1
+console.log(text)
